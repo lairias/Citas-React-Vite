d2649c13234a572b12fdf56a3e1d4e0f0b46a97e
lairias <lairias@unah.hn>
2021-12-25 15:44:53 -0600

    Terminado las View de Formulario y Pacientes

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,7 +1,22 @@
+import { Pacientes } from "./Pacientes";
+
+Pacientes
 const ListadoPacientes = ()=>{
        return (
-         <div className="w-1/2  lg:w-3/5">
-           <h1>Hola desde Lista</h1>
+         <div className="md:w-1/2  lg:w-3/5 md:h-screen overflow-y-scroll">
+           <h2 className="font-black text-3xl text-center">
+             Listado de pacientes
+           </h2>
+           <p className="text-bold mt-5 text-center text-xl  mb-10">
+             Administra tus{" "}
+             <span className="text-indigo-600 font-bold">
+               {" "}
+               Pacientes y Citas
+             </span>
+           </p>
+           <Pacientes />
+           <Pacientes />
+           <Pacientes />
          </div>
        );
 }
---
+++ src/components/Pacientes.jsx
@@ -0,0 +1,22 @@
+
+export const Pacientes = () => {
+    return(
+    <div className="m-3 px-5 py-10 rounded-xl bg-white shadow-md">
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Nombre: <span className="font-normal normal">Hook</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Propietario: <span className="font-normal normal">Hook</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Email: <span className="font-normal normal">Hook</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Fecha de alta: <span className="font-normal normal">Hook</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Síntomas: <span className="font-normal normal">Hook</span>
+      </p>
+    </div>
+    )
+}
